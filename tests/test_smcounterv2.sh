350706e402bb952cd605de1af5ab55b5b281f740
rpadmanabhan <raghus606@gmail.com>
2018-03-28 17:10:16 +0000

    Updated tests

--- tests/test_smcounterv2.sh
+++ tests/test_smcounterv2.sh
@@ -2,8 +2,8 @@
 
 codedir=$1
 
-python $codedir/sm_counter_v2.py --runPath /home/qiauser/test_v2/ --bamName /srv/qgen/test_smcounter-v2/NB956-240-3-10_S1.highconfidence.bam \
-       --bedName /srv/qgen/test_smcounter-v2/high.confidence.variants.bed --prefix NB956-240-3-10_S1.test --nCPU 2 --minBQ 25 \
+python $codedir/sm_counter_v2.py --runPath /home/qiauser/test_v2/ --bamFile /srv/qgen/test_smcounter-v2/NB956-240-3-10_S1.highconfidence.bam \
+       --bedTarget /srv/qgen/test_smcounter-v2/high.confidence.variants.bed --outPrefix NB956-240-3-10_S1.test --nCPU 2 --minBQ 25 \
        --minMQ 50 --hpLen 8 --mismatchThr 6.0 --primerDist 2 --mtThreshold 0.8 --rpb 7.6 --primerSide 1 --minAltUMI 3 --maxAltAllele 2 \
        --refGenome /srv/qgen/data/genome/ucsc.hg19.fa --repBed /srv/qgen/data/annotation/simpleRepeat.full.bed \
        --srBed /srv/qgen/data/annotation/SR_LC_SL.full.bed
